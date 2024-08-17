764fdde38f008dd8a3cedaaf724a8f2d6e547b46
Rajarshee Roy <bitturoy32001@gmail.com>
2024-08-17 20:05:22 +0530

    Order Management+ Third Part Components+ Show Cart Counter+ Social Logins

---
+++ YumBlazor/Services/SharedStateService.cs
@@ -0,0 +1,23 @@
+﻿namespace YumBlazor.Services
+{
+    //this class provides a way to manage and share state of the total cart across different components
+
+    public class SharedStateService
+    {
+        //Onchange is used to notify customer when the state changes
+        public event Action OnChange;
+        private int _totalCartCount;
+
+        public int TotalCartCount
+        {
+            get=> _totalCartCount;
+            set
+            {
+                _totalCartCount = value;
+                NotifyStateChanged();
+            }
+        }
+
+        private void NotifyStateChanged()=> OnChange?.Invoke();
+    }
+}
